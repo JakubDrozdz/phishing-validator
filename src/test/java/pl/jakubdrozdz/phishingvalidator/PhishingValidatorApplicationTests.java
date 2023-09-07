33a93c63ba956ec6671f03f20b60651284e835c5
Jakub Drozdz <jakubekso@outlook.com>
2023-09-07 21:43:33 +0200

    Project init

---
+++ src/test/java/pl/jakubdrozdz/phishingvalidator/PhishingValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakubdrozdz.phishingvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishingValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
